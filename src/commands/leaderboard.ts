73148e6979bb9779102b9eba70675b28bdfb3c78
fanman1 <59294394+fanman1@users.noreply.github.com>
2021-03-20 12:01:57 -0400

    updates to leaderboard
    made for loop. still need to work on sentiment score sorting and getting username

--- src/commands/leaderboard.ts
+++ src/commands/leaderboard.ts
@@ -10,7 +10,14 @@ import * as db from "quick.db";
     ["speckled"= 1.2],
     [fairy= 2],
 ]*/
-
+function cTC(a: (string | number)[], b: (string | number)[]) {
+    if (a[1] === b[1]) {
+        return 0;
+    }
+    else {
+        return (a[1] < b[1]) ? 1 : -1;
+    }
+}
 export default class leaderboard implements IBotCommand {
 
     private readonly aliases = ["leaderboard","lb"]
@@ -31,6 +38,17 @@ export default class leaderboard implements IBotCommand {
     }
 
     async runCommand(args: string[], msg: Discord.Message, Bot: Discord.Client): Promise<void> {
+        let userArray:any=[];
+        let guildArray=msg.guild!.members.cache.array().map(element=>{
+            return element.id
+        })
+        for(const o of db.all()){
+            if(guildArray.includes(o.ID)){
+                userArray.push([o.ID, o.data.sentiment])
+            }
+        }
+        userArray.sort(cTC)
+        console.log(userArray)
         const embed = new Discord.MessageEmbed()
         .setTitle('Positivity Leaderboard!')
         .setDescription('Here are the top ten most positive people in the server!')
@@ -39,13 +57,24 @@ export default class leaderboard implements IBotCommand {
         //.setImage('https://i.redd.it/l28662sbcec51.png')
         .setThumbnail('https://i.imgur.com/aowYZQG.jpeg')
         //.setAuthor(msg.author.username)
-        .addFields(
-          //  { name: usersArray[0], value: usersArray[0] },
-            { name: 'my field title2', value: 'some stuff' },
-            { name: 'my field title3', value: 'some stuff' }
-        )
+        
+        if(msg.guild!.memberCount<10){
+            for(var i=1;i<msg.guild!.memberCount;i++){
+                embed.addFields(
+                  { name: userArray[i][0], value: userArray[i][1]},
+              )
+            }
+        }
+        else{
+            for(var i=1;i<11;i++){
+                embed.addFields(
+                  { name: userArray[i][0], value: userArray[i][1]},
+              )
+            }
+        }
+        
     
-        .setTimestamp()
+        embed.setTimestamp()
     
     msg.channel.send(embed);         
     /*let arr = db.all();
